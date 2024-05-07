bf58399eb01ca72a9683e0037fbe8a97d108dc81
Laura Cespedes <ingcristinacespedes@gmail.com>
2024-05-06 22:49:06 -0500

    add sonarqube

--- src/app/services/Data.service.spec.ts
+++ src/app/services/Data.service.spec.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 import { DataService } from './Data.service';
 
 describe('Service: Data', () => {
